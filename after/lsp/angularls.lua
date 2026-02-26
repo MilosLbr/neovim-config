837b622095821ebec4be7b0a5e328e984961da4a
MilosLbr <brankovicmilos@hotmail.com>
2026-02-26 14:36:52 +0100

    fix for angular ls

--- after/lsp/angularls.lua
+++ after/lsp/angularls.lua
@@ -1,17 +1,15 @@
 return {
-  filetypes = { 'html' },
+  filetypes = { 'htmlangular', 'typescript' },
 
   on_init = function(client)
     -- Disable formatting
     client.server_capabilities.document_formatting = false
     client.server_capabilities.document_range_formatting = false
-
-    -- Disable syntax highlighting
+    --
+    -- -- Disable syntax highlighting
     client.server_capabilities.semantic_tokens_provider = nil
 
     -- Disable "Go to References"
-    -- client.server_capabilities.referencesProvider = false
-
-    -- client.config.capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+    client.server_capabilities.referencesProvider = false
   end,
 }
